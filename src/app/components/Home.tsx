ac8d28b95619d8cbe640308dfcd556ba772048f5
Pearson Grant <pearsongrant23@gmail.com>
2024-07-04 10:12:55 +0200

    commit all

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Modal from 'react-modal'; 
 
 export default function Home() {
   return (
     <>
     <div>Home</div>
     <h1>Home gp</h1>
-</>
+    </>
   )
 }
