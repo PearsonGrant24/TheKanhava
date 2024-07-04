b6f96d63e439e38eb2529395d5848fb896b510c5
Pearson Grant <pearsongrant23@gmail.com>
2024-07-04 14:09:01 +0200

    commit all

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -9,22 +9,22 @@ const MyCollection = [
     {
         label: "1",
         imgPath:
-            "assets/bags.jpg",
+            "assets/pages/img/kanhava/bags.jpg",
     },
     {
         label: "2",
         imgPath:
-            "assets/bags_1.jpg",
+            "assets/pages/img/kanhava/bags_1.jpg",
     },
     {
         label: "3",
         imgPath:
-            "assets/bags_2.jpg",
+            "assets/pages/img/kanhava/bags_2.jpg",
     },
     {
         label: "4 ",
         imgPath:
-            "assets/bags_3.jpg",
+            "assets/pages/img/kanhava/bags_3.jpg",
     },
 ];
 const MyCollection1 = [
